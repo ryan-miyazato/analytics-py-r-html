ba887bd430f51cb13e825e8ff1bcaa1d4553c43a
ryan-miyazato <ryan.miyazato@sptech.school>
2022-11-01 14:38:23 -0300

    graficos atualizando com dados de máquina

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+# pip install psutil
+# pip install rpy2
+# pip install pandas
+# pip install mysql-connector-python
+
+
 # Biblioteca de captura de dados de máquina
 import psutil as ps
+from collections import deque
+from time import sleep
 
 # Bibliotecas para conexão com r
 import pandas as pd
+import rpy2
 import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
@@ -11,22 +20,66 @@ from rpy2.robjects.packages import importr, data
 # Biblioteca de gráficos em r
 import rpy2.robjects.lib.ggplot2 as ggplot2
 
-# Data frame (pandas) de exemplo
-pd_df = pd.DataFrame({'id': [1,2,3],
-                      'valor': [10, 20, 30]})
+# Biblioteca para conexão com mysql
+import mysql.connector as sql
 
-# Conversor de data frame do pandas para o data frame em r
-with localconverter(ro.default_converter + pandas2ri.converter):
-  r_from_pd_df = ro.conversion.py2rpy(pd_df)
+base = importr('base')
 
-grdevices = importr('grDevices')
-grdevices.png(file="/home/aluno/Music/teste-r-html/teste.png", width=1024, height=512)
+cont = 0
 
-pp = (ggplot2.ggplot(r_from_pd_df) +
-    ggplot2.aes_string(x='id', y='valor') +
-    ggplot2.geom_point() +
-    ggplot2.geom_smooth(method = 'lm'))
+ids = deque([])
+cpuPercent = deque([])
+ramPercent = deque([])
+diskPercent = deque([])
 
-pp.plot()
+while cont < 100:
 
-grdevices.dev_off()
+  # Captura de dados de máquina
+
+  ids.append(cont)
+  cpuPercent.append(ps.cpu_percent())
+  ramPercent.append(ps.virtual_memory().percent)
+  diskPercent.append(ps.disk_usage("/").percent)
+
+  print(ids)
+  print(cpuPercent)
+  print(ramPercent)
+  print(diskPercent)
+
+  if cont >= 20:
+    # Data frame (pandas) de exemplo
+    pd_df = pd.DataFrame({'id': ids,
+                          'cpu': cpuPercent,
+                          'ram': ramPercent,
+                          'disk': diskPercent})
+
+    # Conversor de data frame do pandas para o data frame em r
+    with localconverter(ro.default_converter + pandas2ri.converter):
+      dadosMaquina = ro.conversion.py2rpy(pd_df)
+
+    metricas = ["cpu","ram","disk"]
+
+    for metrica in metricas:
+
+      # Gera um .png vazio
+      grdevices = importr('grDevices')
+      grdevices.png(file=f"~/Music/analytics-py-r-html/graficos/{metrica}.png", width=1024, height=512)
+
+      # Plota o gráfico
+      pp = (ggplot2.ggplot(dadosMaquina) +
+          ggplot2.aes_string(x='id', y=metrica) +
+          ggplot2.geom_point() +
+          ggplot2.geom_smooth(method = 'lm'))
+
+      pp.plot()
+
+      # Salva o gráfico no .png
+      grdevices.dev_off()
+
+    ids.popleft()
+    cpuPercent.popleft()
+    ramPercent.popleft()
+    diskPercent.popleft()
+
+  sleep(3)
+  cont += 1
